d75294f8dd254edf6f885c21d0edf3d9b18bbff1
saydinaambiya <ssyyddn@gmail.com>
2022-09-24 23:10:36 +0700

    input n output

---
+++ input_output.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    // int a = 1, b = 2, c = 7;
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << (a + b + c);
+    return 0;
+}
